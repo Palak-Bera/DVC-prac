53c4e4d886fe3e60121c619d9fd1cbd310f84595
Palak Bera <palakbera2281@gmail.com>
2024-07-01 03:06:42 +0530

    Pipeline Versioning added

---
+++ stage_01.py
@@ -0,0 +1,2 @@
+with open("artifact.txt" , "w") as f:
+    f.write("My name is Palak and here executing stage one")
---
+++ stage_03.py
@@ -0,0 +1,2 @@
+with open("artifact2.txt" , "w") as f:
+    f.write("My surname is Bera and here executing stage third")
